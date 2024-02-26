f7af976ee959b9ac5de42341aef17f4c2a9abe16
whhxz <xuzhuowhh@gmail.com>
2024-02-26 14:05:58 +0800

    today

---
+++ new/0100_same-tree.go
@@ -0,0 +1,21 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	} else if p == nil || q == nil {
+		return false
+	} else {
+		return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+	}
+}
+
+func main() {
+	println(isSameTree(nil, nil))
+}
